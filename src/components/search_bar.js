3b2b5aae7c83b28ca5d032f720d39aaff319e26c
Tomek <chojak.t@gmail.com>
2018-04-18 23:07:59 +0200

    adding state to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // This binding is necessary to make `this` work in the callback
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      term: 'search here'
+    };
+  }
+
   render() {
-    return <input onChange={this.onInputChange} />
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <br />
+        Value: {this.state.term}
+      </div>
+    );
   }
 
   onInputChange(e) {
-    console.log(e.target.value);
+    this.setState({term: e.target.value});
+    //console.log(e.target.value);
   }
 
 };
