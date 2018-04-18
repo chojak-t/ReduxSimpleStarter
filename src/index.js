b14b9e32031dca0b5750e7e236de4d9aa6d69721
Tomek <chojak.t@gmail.com>
2018-04-18 23:24:20 +0200

    adding functionality to retrive date

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBIk0BBvCL1MudhwZu4PRwjsRF2pUUGzmo';
+
+YTSearch({key: API_KEY, term: 'aldona orłowska'}, function(data){
+  console.log(data)
+});
 
 /*
 const App = function() {
